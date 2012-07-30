84e14e2afeb06edd4907b9d27506fd944c17dfbd
Faryaab Sheikh <faryaabsheikh@gmail.com>
2012-07-30 05:58:28 -0700

    Separate Config Files

--- products/i9300.mk
+++ products/i9300.mk
@@ -1,8 +1,8 @@
-# Inherit AOSP device configuration for GalaxyS3.
+# Inherit AOSP device configuration.
 $(call inherit-product, device/samsung/i9300/full_i9300.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/supernexus/products/common.mk)
+$(call inherit-product, vendor/supernexus/configs/common.mk)
 
 # Setup device specific product configuration.
 PRODUCT_NAME := GalaxyS3
@@ -14,11 +14,11 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=m0xx TARGET_DEVICE=m0 BUILD_FINGERPRINT="samsung/m0xx/m0:4.0.4/IMM76D/I9300XXALE8:user/release-keys" PRIVATE_BUILD_DESC="m0xx-user 4.0.4 IMM76D I9300XXALE8 release-keys"
 
 # Inherit common build.prop overrides
--include vendor/supernexus/products/common_versions.mk
+-include vendor/supernexus/configs/common_versions.mk
 
 # Copy GalaxyS3 specific prebuilt files
 PRODUCT_COPY_FILES +=  \
     vendor/supernexus/proprietary/i9300/media/bootanimation.zip:system/media/bootanimation.zip 
 
 # Inherit drm blobs
--include vendor/supernexus/products/common_drm_phone.mk
+-include vendor/supernexus/configs/common_drm_phone.mk
